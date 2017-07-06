76f5ea1cd93b9cdbe8cc0f61c565fedb394d3037
Josh Minzner <josh@trialspark.com>
2017-07-06 16:12:18 -0400

    Configure as a library correctly.
    Description
    -----------
    
    Related Pull Requests
    ---------------------
    
    Testing Steps
    -------------

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,32 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './lib'),
     filename: 'react-big-calendar-touch-dnd.js',
+    library: 'reactBigCalendarTouchDnD',
+    libraryTarget: 'umd',
   },
 
-  externals: [
-    'react',
-    'react-dom',
-    /^react-big-calendar\/.+$/,
-  ],
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
+    },
+    'react-big-calendar/lib/utils/accessors': {
+      commonjs: 'react-big-calendar/lib/utils/accessors',
+      commonjs2: 'react-big-calendar/lib/utils/accessors',
+    },
+    'react-big-calendar/lib/utils/propTypes': {
+      commonjs: 'react-big-calendar/lib/utils/propTypes',
+      commonjs2: 'react-big-calendar/lib/utils/propTypes',
+    },
+  },
 
   resolve: {
     extensions: ['.js', '.jsx'],
